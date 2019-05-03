b5ae90f50ebd14d8dcaccf93fa2037c7d3109e9a
Ferencz Dávid Tamás <ferencz.david@protonmail.com>
2019-05-03 18:11:08 +0200

    add a font

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,9 @@
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Anton');
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Anton', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
