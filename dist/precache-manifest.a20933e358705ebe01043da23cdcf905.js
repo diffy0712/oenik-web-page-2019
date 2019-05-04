59732e2b9b383ed7cad4e6f08ba25db2bfd80867
Ferencz Dávid Tamás <ferencz.david@protonmail.com>
2019-05-04 18:08:09 +0200

    first dist

---
+++ dist/precache-manifest.a20933e358705ebe01043da23cdcf905.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "74104a4ab2891c180a42",
+    "url": "css/about.6299d719.css"
+  },
+  {
+    "revision": "74104a4ab2891c180a42",
+    "url": "js/about.0b9ba6e7.js"
+  },
+  {
+    "revision": "5c968ba3e4b9d8beafe9",
+    "url": "css/app.f5ac4a8a.css"
+  },
+  {
+    "revision": "5c968ba3e4b9d8beafe9",
+    "url": "js/app.a8fd275e.js"
+  },
+  {
+    "revision": "bb0517a8db953fbbc0ea",
+    "url": "js/chunk-vendors.c2a7e30a.js"
+  },
+  {
+    "revision": "dd71029c81000fd65374cf2504c895f7",
+    "url": "img/blog_01.dd71029c.jpg"
+  },
+  {
+    "revision": "150c3555655fe4f7a611c560b308e2fd",
+    "url": "img/blog_02.150c3555.jpg"
+  },
+  {
+    "revision": "3cdd1c771d4892497c629431438153db",
+    "url": "img/about_2.3cdd1c77.jpg"
+  },
+  {
+    "revision": "21ab4f0e4f24067eb844c9fd4e940698",
+    "url": "img/cliff_1.21ab4f0e.jpg"
+  },
+  {
+    "revision": "a135f55f06243a80a54c3adffc9fcf95",
+    "url": "img/ice.a135f55f.jpg"
+  },
+  {
+    "revision": "6019fe07af9620fb1b143277791afe76",
+    "url": "img/blog_05.6019fe07.jpg"
+  },
+  {
+    "revision": "e58092f4822dd0de652a983e7a4b1f40",
+    "url": "img/blog_03.e58092f4.jpg"
+  },
+  {
+    "revision": "1965e7b91f7061bf0e55a37c94ecd9ff",
+    "url": "img/cliff.1965e7b9.jpg"
+  },
+  {
+    "revision": "4929ede637b3c688f2ee55235bbb7315",
+    "url": "img/about.4929ede6.jpg"
+  },
+  {
+    "revision": "6e640b83d8c78ad5ffccde7665734f5a",
+    "url": "img/blog_04.6e640b83.jpg"
+  },
+  {
+    "revision": "2457558ceee367fbeeb9aa982b7a5e76",
+    "url": "img/lake.2457558c.jpg"
+  },
+  {
+    "revision": "00ae00170e398a542b11eb2a8e98266a",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "9ea8f4eddad0f2893e3d4502a350ee5b",
+    "url": "api/posts.json"
+  }
+];
